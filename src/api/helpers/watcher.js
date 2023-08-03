15fe959187a6453535b1702e5cbaff278a1ef5a0
muh rifai <muhrifai554@gmail.com>
2023-08-03 09:43:19 +0700

    Move file when error

--- src/api/helpers/watcher.js
+++ src/api/helpers/watcher.js
@@ -622,14 +622,14 @@ watcher.on("add", async (path) => {
         });
       } catch (error) {
         console.log(error, "error'");
-        // const newFileName = `${failed_folder}/${fileName}`;
-        // fs.renameSync(path, newFileName, (err) => {
-        //   if (err) {
-        //     console.log(`Error while moving Failed file : ${err.message}`);
-        //   } else {
-        //     console.log(`Failed to process and moved file to: ${newFileName}`);
-        //   }
-        // });
+        const newFileName = `${failed_folder}/${fileName}`;
+        fs.renameSync(path, newFileName, (err) => {
+          if (err) {
+            console.log(`Error while moving Failed file : ${err.message}`);
+          } else {
+            console.log(`Failed to process and moved file to: ${newFileName}`);
+          }
+        });
       }
     }, 800);
   }
